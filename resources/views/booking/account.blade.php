251fefb4d9bc61a3efabf38aed8755f35644ec7f
keyesa964 <keyesa.aja@icloud.com>
2023-11-20 14:33:40 +0700

    20/11/23

---
+++ resources/views/booking/account.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="globals.css" />
+        <link rel="stylesheet" href="styleguide.css" />
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div class="iphone-pro-max">
+            <div class="overlap">
+                <div class="rectangle"></div>
+                <img class="ellipse" src="img/ellipse-5.png" />
+                <div class="back"><img class="arrow" src="img/arrow-3.svg" /></div>
+            </div>
+            <div class="overlap-group">
+                <div class="div"></div>
+                <div class="text-wrapper">Booking Schedule</div>
+            </div>
+            <img class="line" src="img/line-55.svg" />
+            <img class="img" src="img/line-56.svg" />
+            <img class="line-2" src="img/line-57.svg" />
+            <img class="line-3" src="img/line-58.svg" />
+            <img class="line-4" src="img/line-59.svg" />
+            <div class="text-wrapper-2">Your name here</div>
+            <div class="text-wrapper-3">Talent</div>
+            <p class="p">Bio (Tell me abaout yourself )</p>
+            <div class="text-wrapper-4">E-Mail</div>
+            <div class="text-wrapper-5">Media Links</div>
+            <button class="button"><div class="text-wrapper-6">Logout</div></button>
+            <button class="overlap-group-wrapper">
+                <div class="overlap-group-2">
+                    <div class="rectangle-2"></div>
+                    <div class="text-wrapper-7">Save</div>
+                </div>
+            </button>
+        </div>
+    </body>
+</html>
